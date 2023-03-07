7459a73691689cf9aca3ea830fbc34c74c9b2420
Marc Català <mcatala@seidor.com>
2023-03-07 14:30:45 +0100

    New release v1.1.2

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_c4l';
-$plugin->release = '1.1.1';
+$plugin->release = '1.1.2';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2023012901;
+$plugin->version = 2023012902;

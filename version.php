9cf8f9efdc624512944bee4c35f5b6aa41e820f0
Marc Català <reskit@gmail.com>
2024-10-22 16:55:43 +0200

    New release v3.0.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_c4l';
-$plugin->release = '3.0.0';
+$plugin->release = '3.0.1';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2024042900;
+$plugin->version = 2024042901;
